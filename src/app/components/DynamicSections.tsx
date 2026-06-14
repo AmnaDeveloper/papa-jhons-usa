4d451c325c85cebff50862e925df23f3e426e005
AmnaDeveloper <amnasadam2003@gmail.com>
2026-06-14 12:59:40 +0500

    Update homepage: remove promo code section and update guides heading & description

--- src/app/components/DynamicSections.tsx
+++ src/app/components/DynamicSections.tsx
@@ -4,14 +4,12 @@ import dynamic from 'next/dynamic';
 
 const MenuGuideSection = dynamic(() => import("./MenuGuideSection"), { ssr: false });
 const RestaurantHoursSection = dynamic(() => import("./RestaurantHoursSection"), { ssr: false });
-const CouponsSection = dynamic(() => import("./CouponsSection"), { ssr: false });
 
 export default function DynamicSections() {
     return (
         <>
             <MenuGuideSection />
             <RestaurantHoursSection />
-            <CouponsSection />
         </>
     );
 }
